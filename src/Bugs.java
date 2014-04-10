65a682a589172a7634b549e2c8afbfc5a2a0d047
Nikolai <Nikolai@192.168.1.77>
2014-04-10 18:20:11 +0200

    FFT semple ok, prob de gain

---
+++ src/Bugs.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * j'ai fais une classe bug juste pour que les todo soit trouvé par netbean
+ * @author Nikolai
+ */
+public class Bugs {
+ /*   @todo BUGS
+
+BUG 1 :
+
+Si la grille n'est pas carré ça pose problème 
+
+Probleme de clique avec la liste de bateaux
+
+Ca lag quand la grille est grande et les buttons sont grand
+    
+    
+@todo thomas cf test3 testDao, la liste renvoyé ne contient que des games nulls
+    
+    */
+}
