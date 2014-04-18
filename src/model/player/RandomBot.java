45ff92249d8cc1e4affa1c073dc0b1c93867e731
Rym_r <rym.rabehi@gmail.com>
2014-04-19 01:05:47 +0200

    Merge origin/master
    Conflicts:
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties

--- src/model/player/RandomBot.java
+++ src/model/player/RandomBot.java
@@ -9,6 +9,7 @@ package model.player;
 import java.io.Serializable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.Random;
 import model.Coordinate;
 import model.StateCase;
 import static model.StateCase.ERROR;
@@ -33,8 +34,11 @@ public class RandomBot extends Bot implements Serializable{
             int l = (int)(Math.random());
             Coordinate c = new Coordinate(x ,y);
             lastCoordinateFired = c;
-            OrdreTir o = new OrdreTir(c, 0);
+            
             int nbShip = target.getVaisseaux().size();
+            Random rand = new Random();
+            int nb = rand.nextInt(nbShip)+1;
+            OrdreTir o = new OrdreTir(c, nb);
             return flotte.fire(target, o);
         } catch (Exception ex) {
             Logger.getLogger(RandomBot.class.getName()).log(Level.SEVERE, null, ex);
